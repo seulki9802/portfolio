af12a4d27725716b4990d15b5e3e54e1bfeed3d7
seulki9802 <calop@naver.com>
2022-05-19 21:40:41 +0900

    프로젝트 페이지 구상

--- src/page/Project.js
+++ src/page/Project.js
@@ -1,11 +1,46 @@
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2
+    }
+  }
+};
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1
+  }
+};
+
 function Project() {
+  return(
+    <div className="Project">
 
-  return (
-    <div>
-      sdfs
-    </div>
-  );
+      <motion.ul
+      className="Project-container"
+      variants={container}
+      initial="hidden"
+      animate="visible"
+    >
+      {[1, 2, 3].map((i, index) => (
+        <motion.li key={index} className="Project-item" variants={item}>
+          { i }
+        </motion.li>
+      ))}
+    </motion.ul>
+    
+  </div>
+  )
 }
-
-
+  
+  
 export default Project;
+  
