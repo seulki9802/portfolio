c6c6227f7487883e148e7f7047fc1e511a8f7856
seulki9802 <calop@naver.com>
2022-05-23 17:56:10 +0900

    인트로 자동으로 넘어가기 만들기. setInterval을 사용하면 안 됨. 왜냐면 인트로가 바뀌는 건 현재 인트로가 바뀌면서 재랜더링이 되는 거기 때문에 setInterval하면 setInterval이 여러개 생김. 그래서 setTimeout을 해줘야하는데 이 것도 쓰고 나면 혹은 버튼을 눌렀을 때(동작하길 원치 않음)는 clearTimeout을 써서 확실히 폐기시켜 줘야함.

--- src/page/Intro.js
+++ src/page/Intro.js
@@ -2,83 +2,93 @@ import { Link } from 'react-router-dom';
 import {CSSTransition, SwitchTransition} from "react-transition-group";
 import { useState } from 'react';
 
-import logo from '../logo.svg';
+// import logo from '../logo.svg';
 
 function Intro({ setPage }) {
 
-    const intros = [
-      {
-        id : 0,
-        title: 'HI!',
-        description: 'Hi, I\'m SeulKi',
-        link : '/',
-        background: 'white'
-      },
-      {
-        id : 1,
-        title: 'SEULKI SEULKI SEULKI SEULKI SEULKI SEULKI',
-        description: 'Hi, I\'m SeulKi',
-        link : '/me',
-        background: 'green'
-      },
-      {
-        id : 2,
-        title: 'MY PROJECTS! MY PROJECTS!',
-        description: 'I studied Python, JS, HTML, React, NodeJs',
-        link : '/project',
-        background: 'red'
-      },
-      {
-        id : 3,
-        title: 'INTERSTED?INTERSTED?INTERSTED?INTERSTED?INTERSTED?INTERSTED?INTERSTED?INTERSTED?INTERSTED?INTERSTED?INTERSTED?',
-        description: 'Contac me!',
-        link : '/contact',
-        background: 'yellow'
-      }
-    ]
-  
-    const [intro, setIntro] = useState(intros[0])
-    
-    function changeIntro(e){
-      setIntro(intros[e.target.innerHTML])
+  const intros = [
+    {
+      id : 0,
+      title: 'HI!',
+      description: 'Hi, I\'m SeulKi',
+      link : '/',
+      background: 'white'
+    },
+    {
+      id : 1,
+      title: 'SEULKI SEULKI SEULKI SEULKI SEULKI SEULKI',
+      description: 'Hi, I\'m SeulKi',
+      link : '/me',
+      background: 'green'
+    },
+    {
+      id : 2,
+      title: 'MY PROJECTS! MY PROJECTS!',
+      description: 'I studied Python, JS, HTML, React, NodeJs',
+      link : '/project',
+      background: 'red'
+    },
+    {
+      id : 3,
+      title: 'INTERSTED?INTERSTED?INTERSTED?INTERSTED?INTERSTED?INTERSTED?INTERSTED?INTERSTED?INTERSTED?INTERSTED?INTERSTED?',
+      description: 'Contac me!',
+      link : '/contact',
+      background: 'yellow'
     }
+  ]
 
-    function changePage(e){
-      setPage(e.target.id)
-    }
+  const [intro, setIntro] = useState(intros[0])
 
-    return(
-      <div className="Intro">
-        <SwitchTransition mode={ "out-in" }>
-
-          <CSSTransition
-            key={ intro.id }
-            addEndListener={(node, done) => node.addEventListener("transitionend", done, false)}
-            classNames="Intro-transition"
-          >
-            <div>
-              <h1 className="Intro-logo">{ intro.id }</h1>
-              <img src={logo} className="Intro-logo" alt="logo" />
-              
-              <h1>{ intro.title }</h1>
-              <p>{ intro.description }</p>
-              <Link to={ intro.link } id={ intro.id } onClick={ changePage }>Lear More</Link>
-            </div>
-            
-          </CSSTransition>
-        
-        </SwitchTransition>
-
-        <div>
-          {intros.map((intro) => {
-            return <button key={ intro.id } onClick={ changeIntro }>{ intro.id }</button>
-            })}
-        </div>
-  
-  
-      </div>
-    )
-  
+  function changeIntro(e){
+    clearTimeout(streamIntro)
+    setIntro(intros[e.target.innerHTML])
   }
-  
-  export default Intro;
+
+  function changePage(e){
+    setPage(e.target.id)
+  }
+
+  const streamIntro = setTimeout(() => {
+    var now = intro.id
+    var next;
+    if ( now === 3 ) next = 0;
+    else next = now + 1;
+    clearTimeout(streamIntro)
+    setIntro(intros[next])
+  }, 2000)
+
+  return(
+    <div className="Intro">
+      <SwitchTransition mode={ "out-in" }>
+
+        <CSSTransition
+          key={ intro.id }
+          addEndListener={(node, done) => node.addEventListener("transitionend", done, false)}
+          classNames="Intro-transition"
+        >
+          <div>
+            <h1 className="Intro-logo">{ intro.id }</h1>
+            {/* <img src={logo} classNamz="Intro-logo" alt="logo" /> */}
+            
+            <h1>{ intro.title }</h1>
+            <p>{ intro.description }</p>
+            <Link to={ intro.link } id={ intro.id } onClick={ changePage }>Lear More</Link>
+          </div>
+          
+        </CSSTransition>
+      
+      </SwitchTransition>
+
+      <div>
+        {intros.map((intro) => {
+          return <button key={ intro.id } onClick={ changeIntro }>{ intro.id }</button>
+          })}
+      </div>
+
+
+    </div>
+  )
+
+}
+
+export default Intro;
