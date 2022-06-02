5e1be70f42cf5404464d7adb38718b68fe5ab18e
seulki9802 <calop@naver.com>
2022-06-02 12:56:41 +0900

    프로젝트 리스트 클릭하면 보여주는 화면 추가. 버튼(x, next, before) 추가

--- src/page/Project.js
+++ src/page/Project.js
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { motion } from "framer-motion";
+
 
 const container = {
   hidden: { opacity: 1, scale: 0 },
@@ -8,7 +10,7 @@ const container = {
     transition: {
       delayChildren: 0.3,
       staggerChildren: 0.3
-    }
+    },
   }
 };
 
@@ -16,31 +18,53 @@ const item = {
   hidden: { y: 20, opacity: 0 },
   visible: {
     y: 0,
-    opacity: 1
+    opacity: 1,
   }
 };
 
 function Project() {
-  
+
   document.querySelector('body').style.backgroundColor = '#D59BF6';
+
+  const [show, setShow] = useState(false);
 
   return(
     <div className="Project">
 
-      <motion.ul
-      className="Project-container"
-      variants={container}
-      initial="hidden"
-      animate="visible"
+      {show
+      ? <ProjectShow setShow={ setShow } />
+      : <PorjectList setShow={ setShow }/>}
+
+    
+  </div>
+  )
+}
+
+function PorjectList({ setShow }) {
+  return(
+    <motion.ul
+    className="Project-list-container"
+    variants={container}
+    initial="hidden"
+    animate="visible"
     >
       {['게임, 랭킹', '메모, 채팅', '커뮤니티?', '내 포트폴리오'].map((i, index) => (
-        <motion.li key={index} className="Project-item" variants={item}>
+        <motion.li key={index} className="Project-item" variants={item} onClick={ () => setShow(true) }>
           { i }
         </motion.li>
       ))}
     </motion.ul>
-    
-  </div>
+  )
+}
+
+function ProjectShow({ setShow }) {
+  return(
+    <motion.div className='Project-show-container' initial={{ scale: 0 }} animate={{ scale :1 }}>
+      dpdpdpdpdppddppdpd
+      <button className='Project-show-before'>-</button>
+      <button className='Project-show-after'>+</button>
+      <button className='Project-show-exit' onClick={ () => setShow(false) }>x</button>
+    </motion.div>
   )
 }
   
