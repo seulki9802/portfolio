30c030e6e5c238dcd4c5e1345086914c5e764a33
seulki9802 <calop@naver.com>
2022-06-16 18:59:12 +0900

    프로젝트 사진 추가

--- src/page/Project.js
+++ src/page/Project.js
@@ -4,6 +4,9 @@ import { motion } from "framer-motion";
 import project0 from '../assets/project_donut0.jpeg'
 import project01 from '../assets/project_donut1.jpeg'
 import project02 from '../assets/project_donut2.jpeg'
+import project03 from '../assets/project_donut3.jpeg'
+import project04 from '../assets/project_donut4.jpeg'
+
 
 import project1 from '../assets/project_board0.jpeg'
 import project11 from '../assets/project_board1.jpeg'
@@ -11,7 +14,11 @@ import project12 from '../assets/project_board2.jpeg'
 import project13 from '../assets/project_board3.jpeg'
 import project14 from '../assets/project_board4.jpeg'
 
-import project2 from '../assets/project_portfolio0.png'
+import project2 from '../assets/project_portfolio0.jpeg'
+import project21 from '../assets/project_portfolio1.jpeg'
+import project22 from '../assets/project_portfolio2.jpeg'
+import project23 from '../assets/project_portfolio3.jpeg'
+
 
 import git from '../assets/git.png'
 import home from '../assets/home.png'
@@ -47,7 +54,7 @@ const projects = [
     introduction: '채소를 피해 도넛을 먹는 게임!',
     skills: 'HTML, JavaScript, Node.js, MongoDB', 
     description: '\v 키보드로 동작하며 점수를 올려 랭킹을 갱신하는 게임입니다.\n\n\v 게임 제작을 위해 html의 canvas 태그를 이용하고 랭킹 시스템을 위해 Node.js와 mongoDB를 사용하였습니다.\n\n\v 이 게임은 이전에 html과 js만으로 제작을 완료하였지만 React와 Node.js를 연결하는 데 공부하기 위하여 프로젝트를 재구성하여 만들었습니다.\n\n\v React로 게임의 첫 화면인 닉네임을 입력하는 페이지와 랭킹 확인 페이지를 제작하였으며 페이지의 요소를 나누어 제작할 수 있는 React의 편리성과 장점을 체감할 수 있었습니다.' ,
-    img: [ { id: 0, img: project0 }, { id: 1, img: project01 }, { id: 2, img: project02 } ]
+    img: [ { id: 0, img: project0 }, { id: 1, img: project01 }, { id: 2, img: project02 }, { id: 3, img: project03 }, {id: 4, img: project04 } ]
   },
   {
     id: 1,
@@ -67,7 +74,8 @@ const projects = [
     introduction: 'portfolio',
     skills: 'JavaScript, React, Node.js, MongoDB, CSS',
     description: '\v 저의 가치관과 개성을 담을 수 있는 페이지를 만들었습니다.\n\n\v 저에게 웹 페이지란 가독성 좋은 문서이자 사용자가 경험할 수 있는 문서입니다. 기존의 문서들은 페이지들 간의 상호작용이 어렵다는 단점이 있습니다. 그러나 웹은 이를 극복하고 많은 문서를 보다 구조적으로 손 쉽게 열람할 수 있도록 합니다.\n\n\v 이를 위해 저는 페이지를 여러 개로 나누고 페이지간의 이동이 용이하도록 하며 애니메이션을 추가하여 사용자의 경험에 도움이 될 수 있도록 구성하였습니다. \n\n\v React를 처음 공부하며 만든 프로젝트이며 페이지를 나누고 요소들을 나누면서 React를 공부하고 React의 장점과 단점을 경험할 수 있는 프로젝트였습니다.',
-    img: [{ id:0, img: project2 }]
+    img: [ { id: 0, img: project2 }, { id: 1, img: project21 }, { id: 2, img: project22 }, { id: 3, img: project23 } ]
+
   }
 ]
 
@@ -106,7 +114,7 @@ function PorjectList({ setShow, setProject }) {
 
       {projects.map((project) => (
         <motion.li key={ project.id } className="Project-item" variants={ item } onClick={ () => projectShow(project.id) }>
-          <img src={ project.img[0].img } alt={ project.title } ></img>
+          <img src={ project.img[1].img } alt={ project.title } ></img>
           <h3>{ project.title }</h3>
           <p>{ project.introduction }</p>
 
